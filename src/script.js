6dcee589815c9f03b6a2836643238b2bba258630
guy <guy_wichapad@hotmail.com>
2023-10-31 17:16:30 +0700

    update carousel portfolio

--- src/script.js
+++ src/script.js
@@ -46,7 +46,46 @@ function sendMail() {
       document.getElementById("subject").value = "";
       document.getElementById("message").value = "";
       console.log(res);
-      alert("Thank you, " + params['name'] + ' ! Your message has been sent');
+      alert("Thank you, " + params["name"] + " ! Your message has been sent");
     })
     .catch((err) => console.log(err));
 }
+
+// Portfolio
+const carousel = document.querySelector(".carousel-portfolio");
+const arrowBtns = document.querySelectorAll("#wrapper button");
+const firstCardWidth = carousel.querySelector(
+  ".container-image-portfolio"
+).offsetWidth;
+
+// Add click arrows prev next image
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+// (Draging)
+let isDragging = false,
+  startX,
+  startScrollLeft;
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
