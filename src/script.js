9760e967d4c22bd65fb1f563f483cf020a1d523e
guy <guy_wichapad@hotmail.com>
2023-11-02 10:46:20 +0700

    update code

--- src/script.js
+++ src/script.js
@@ -52,40 +52,41 @@ function sendMail() {
 }
 
 // Portfolio
-const carousel = document.querySelector(".carousel-portfolio");
-const arrowBtns = document.querySelectorAll("#wrapper button");
-const firstCardWidth = carousel.querySelector(
-  ".container-image-portfolio"
-).offsetWidth;
+// (Carousel)
+// const carousel = document.querySelector(".carousel-portfolio");
+// const arrowBtns = document.querySelectorAll("#wrapper button");
+// const firstCardWidth = carousel.querySelector(
+//   ".container-image-portfolio"
+// ).offsetWidth;
 
-// Add click arrows prev next image
-arrowBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
-  });
-});
+// // Add click arrows prev next image
+// arrowBtns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+//   });
+// });
 
 // (Draging)
-let isDragging = false,
-  startX,
-  startScrollLeft;
+// let isDragging = false,
+//   startX,
+//   startScrollLeft;
 
-const dragStart = (e) => {
-  isDragging = true;
-  carousel.classList.add("dragging");
-  startX = e.pageX;
-  startScrollLeft = carousel.scrollLeft;
-};
+// const dragStart = (e) => {
+//   isDragging = true;
+//   carousel.classList.add("dragging");
+//   startX = e.pageX;
+//   startScrollLeft = carousel.scrollLeft;
+// };
 
-const dragging = (e) => {
-  if (!isDragging) return;
-  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-};
-const dragStop = () => {
-  isDragging = false;
-  carousel.classList.remove("dragging");
-};
+// const dragging = (e) => {
+//   if (!isDragging) return;
+//   carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+// };
+// const dragStop = () => {
+//   isDragging = false;
+//   carousel.classList.remove("dragging");
+// };
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
+// carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("mousemove", dragging);
+// document.addEventListener("mouseup", dragStop);
